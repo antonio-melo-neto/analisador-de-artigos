6af8fa6da6ba79905cb748a1b3fe1b2b4c8e6a1c
antonio-melo-neto <antoniodemelo@gmail.com>
2024-10-28 17:52:14 -0300

    Update API base URL for backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // URL do backend FastAPI
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://analisador-backend.onrender.com' // URL do backend hospedado
+    : 'http://127.0.0.1:8000',                  // URL local para desenvolvimento
 });
 
 export default api;
