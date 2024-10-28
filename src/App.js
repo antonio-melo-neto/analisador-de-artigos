7f75f2c5069b7f528c62cf083b2661284c0d3618
antonio-melo-neto <antoniodemelo@gmail.com>
2024-10-27 21:37:43 -0300

    First commit: upload frontend React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/Home'; // Importa a página Home
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* Renderiza a página Home */}
+    </div>
+  );
+}
+
+export default App;
