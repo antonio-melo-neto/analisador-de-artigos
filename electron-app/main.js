7b331c0936adea9f93b47a3d0c0cf9f5d096d8a4
antonio-melo-neto <antoniodemelo@gmail.com>
2024-10-31 17:07:28 -0300

    Atualização da URL base do api.js para backend público

---
+++ electron-app/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // Carrega o build do React (depois de compilar o frontend)
+    win.loadFile(path.join(__dirname, '../frontend/build/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
